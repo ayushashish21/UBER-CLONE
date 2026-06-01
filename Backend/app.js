5f0b68a7bad6dae285a809b12361eef46333c0a5
Ayush Ashish <ayushashish52@gmail.com>
2026-06-01 09:50:59 +0530

    register captain done

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,7 @@ const app = express();
 const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
+const captainRoutes = require('./routes/captain.routes');
 
 
 connectToDb();
@@ -19,6 +20,11 @@ app.use(cookieParser());
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
 app.use('/users', userRoutes);
+app.use('/captains', captainRoutes);
+
+
+
 
 module.exports = app;
